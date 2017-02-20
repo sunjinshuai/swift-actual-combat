b3761e200bdcefb5d539c5d5ad57f32f1ae4c179
sunjinshuai <s_mike@163.com>
2017-02-20 23:15:36 +0800

    项目搭建

--- JSWeiboDemo/Classes/Main/Controller/WBTabBarController.swift
+++ JSWeiboDemo/Classes/Main/Controller/WBTabBarController.swift
@@ -10,8 +10,38 @@ import UIKit
 
 class WBTabBarController: UITabBarController {
 
-    // MARK: - 私有控件 撰写按钮
-    fileprivate lazy var composeBtn : UIButton = UIButton.yw_imageButton("tabbar_compose_icon_add", backgroundImageName: "tabbar_compose_button")
-    
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 实例化自定义tabBar
+        let tabBar = WBTabBar()
+
+        // 通过kvc 给只读属性赋值
+        setValue(tabBar, forKey: "tabBar")
+        // 首页
+        addChildViewController(HomeViewController(), title: "首页", imgName: "tabbar_home")
+        // 消息
+        addChildViewController(MessageViewController(), title: "消息", imgName: "tabbar_message_center")
+        // 发现
+        addChildViewController(DiscoverViewController(), title: "发现", imgName: "tabbar_discover")
+        // 我
+        addChildViewController(ProfileViewController(), title: "我", imgName: "tabbar_profile")
+    }
+}
+
+// MARK: - 添加子控制器的公共方法时
+extension WBTabBarController {
+
+    func addChildViewController(_ childController: UIViewController, title: String, imgName: String) {
+        // 设置title
+        childController.title = title
+        // 设置title颜色
+        childController.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName: WBTHEMECOLOR], for: UIControlState.selected)
+        // 设置image
+        // withRenderingMode 渲染方式
+        // alwaysOriginal 采用图片原生颜色
+        childController.tabBarItem.image = UIImage(named: imgName)?.withRenderingMode(UIImageRenderingMode.alwaysOriginal)
+        childController.tabBarItem.selectedImage = UIImage(named: "\(imgName)_selected")?.withRenderingMode(UIImageRenderingMode.alwaysOriginal)
+        // 添加子控制器
+        addChildViewController(UINavigationController(rootViewController: childController))
+    }
 }
---
+++ JSWeiboDemo/Classes/Public/Constant/Constant.swift
@@ -0,0 +1,16 @@
+//
+//  Constant.swift
+//  JSWeiboDemo
+//
+//  Created by Michael on 2017/2/20.
+//  Copyright © 2017年 com.51fanxing. All rights reserved.
+//
+
+import UIKit
+
+// 屏幕的宽度和高度
+let WBSCREENWIDTH = UIScreen.main.bounds.width
+let WBSCREENHEIGHT = UIScreen.main.bounds.height
+
+// 微博的主题颜色
+let WBTHEMECOLOR = UIColor.orange
