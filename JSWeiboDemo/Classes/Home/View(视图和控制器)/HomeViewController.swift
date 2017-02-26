7d4bc0754abe80ee9bbc93fad34bdeec9fb0782a
sunjinshuai <s_mike@163.com>
2017-02-26 10:53:55 +0800

    setupNavigationItem

--- JSWeiboDemo/Classes/Home/View(视图和控制器)/HomeViewController.swift
+++ JSWeiboDemo/Classes/Home/View(视图和控制器)/HomeViewController.swift
@@ -10,4 +10,33 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupNavigationItem()
+    }
+    
+    //MARK: - 设置导航
+    private func setupNavigationItem() {
+        // 左侧
+        navigationItem.leftBarButtonItem =  UIBarButtonItem(imgName: "navigationbar_friendsearch", target: self, action: #selector(leftClick))
+        // 右侧
+        navigationItem.rightBarButtonItem = UIBarButtonItem(imgName: "navigationbar_pop", target: self, action: #selector(rightClick))
+    }
+}
+
+//MARK: - 监听事件
+extension HomeViewController {
+    /*
+     private 私有的在自己类内部使用没有问题 但是 如果吧方法放到其类的extension 就会找不到该方法 需要使用fileprivate 来修饰
+     */
+    // 左侧按钮点击
+    @objc fileprivate func leftClick(){
+        print("左侧按钮点击")
+    }
+    // 右侧按钮点击
+    @objc fileprivate func rightClick(){
+        print("右侧按钮点击")
+        
+    }
 }
