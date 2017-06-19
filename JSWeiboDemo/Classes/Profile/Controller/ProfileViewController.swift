7f46e65e18fa04a0a2753641e43e3c51b58b748e
孙金帅 <s_mike@163.com>
2017-06-19 20:18:51 +0800

    个人中心

--- JSWeiboDemo/Classes/Profile/Controller/ProfileViewController.swift
+++ JSWeiboDemo/Classes/Profile/Controller/ProfileViewController.swift
@@ -15,16 +15,14 @@ class ProfileViewController: UIViewController {
     
     fileprivate var tableView: UITableView?
     
-    fileprivate var listArr = [[""],["签到有礼","新的好友"],["我的相册","我的赞"],["微博钱包","微博运动","免流量"],["粉丝服务","粉丝头条"],["客服中心"],["草稿箱"]]
-    fileprivate var listExplainArr = [[""],["每日签到，红包多多",""],["","收藏，移到这里了"],["送钱！火力全开","全民热舞抢红包","刷微博、看视频"],["写文章、发点评、赚粉丝","推广博文及账号的利器"],[""],[""]]
+    fileprivate var listArray: Array<Any> = []
     
-    fileprivate var listImageArr = [[""],["message_drafts","message_addfansgroup"],["message_album","message_fabulous"],["message_wallet","message_motion","message_flow"],["message_fans","message_the_headlines"],["message_customer_service"],["message_drafts"]]
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupNavigationItem()
         setupTableView()
+        loadNetWorkRequest()
     }
     
     private func setupNavigationItem() {
@@ -37,11 +35,11 @@ class ProfileViewController: UIViewController {
 extension ProfileViewController: UITableViewDataSource, UITableViewDelegate {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return listArr.count
+        return listArray.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return listArr[section].count
+        return (listArray[section] as AnyObject).count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -97,5 +95,106 @@ extension ProfileViewController {
         view.insertSubview(tableView!, belowSubview: (self.navigationController?.navigationBar)!)
     }
 }
+
+/// MARK: - 设置数据
+extension ProfileViewController {
+    
+    /// 设置tableView
+    func loadNetWorkRequest() {
+        
+        var group1 = Array<Any>()
+        
+        let model1 = ProfileModel()
+        model1.profile_titleName = "签到有礼"
+        model1.profile_image = "message_motion"
+        model1.profile_subTitleName = "每日签到，红包多多"
+        
+        let model2 = ProfileModel()
+        model2.profile_titleName = "新的好友"
+        model2.profile_image = "message_addfansgroup"
+        model2.profile_subTitleName = ""
+        
+        group1.append(model1)
+        group1.append(model2)
+        
+        var group2 = Array<Any>()
+        
+        let model3 = ProfileModel()
+        model3.profile_titleName = "我的相册"
+        model3.profile_image = "message_album"
+        model3.profile_subTitleName = ""
+        
+        let model4 = ProfileModel()
+        model4.profile_titleName = "我的赞"
+        model4.profile_image = "message_fabulous"
+        model4.profile_subTitleName = "收藏，移到这里了"
+        
+        group2.append(model3)
+        group2.append(model4)
+        
+        var group3 = Array<Any>()
+        
+        let model5 = ProfileModel()
+        model5.profile_titleName = "微博钱包"
+        model5.profile_image = "message_wallet"
+        model5.profile_subTitleName = "送钱！火力全开"
+        
+        let model6 = ProfileModel()
+        model6.profile_titleName = "微博运动"
+        model6.profile_image = "message_motion"
+        model6.profile_subTitleName = "全民热舞抢红包"
+        
+        let model7 = ProfileModel()
+        model7.profile_titleName = "免流量"
+        model7.profile_image = "message_flow"
+        model7.profile_subTitleName = "刷微博、看视频"
+        
+        group3.append(model5)
+        group3.append(model6)
+        group3.append(model7)
+        
+        var group4 = Array<Any>()
+        
+        let model8 = ProfileModel()
+        model8.profile_titleName = "粉丝服务"
+        model8.profile_image = "message_fans"
+        model8.profile_subTitleName = "写文章、发点评、赚粉丝"
+        
+        let model9 = ProfileModel()
+        model9.profile_titleName = "粉丝头条"
+        model9.profile_image = "message_the_headlines"
+        model9.profile_subTitleName = "推广博文及账号的利器"
+        
+        group4.append(model8)
+        group4.append(model9)
+        
+        var group5 = Array<Any>()
+        
+        let model10 = ProfileModel()
+        model10.profile_titleName = "客服中心"
+        model10.profile_image = "message_customer_service"
+        model10.profile_subTitleName = ""
+        
+        group5.append(model10)
+        
+        var group6 = Array<Any>()
+        
+        let model11 = ProfileModel()
+        model11.profile_titleName = "草稿箱"
+        model11.profile_image = "message_drafts"
+        model11.profile_subTitleName = ""
+        
+        group6.append(model11)
+        
+        listArray.append(group1)
+        listArray.append(group2)
+        listArray.append(group3)
+        listArray.append(group4)
+        listArray.append(group5)
+        listArray.append(group6)
+    }
+}
+
+
 
 
--- JSWeiboDemo/Classes/Profile/View/ProfileViewCell.swift
+++ JSWeiboDemo/Classes/Profile/View/ProfileViewCell.swift
@@ -12,13 +12,79 @@ class ProfileViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
-
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: UITableViewCellStyle.default, reuseIdentifier: reuseIdentifier)
+        
+        setupUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
-
+    
+    func setupUI() {
+        
+        addSubview(avatarImageView)
+        addSubview(titleLable)
+        addSubview(subTitleLable)
+        addSubview(arrowImageView)
+        
+        avatarImageView.snp.makeConstraints { (make) in
+            make.top.equalTo(15)
+            make.left.equalTo(15)
+            make.width.height.equalTo(20)
+        }
+        
+        titleLable.snp.makeConstraints { (make) in
+            make.left.equalTo(avatarImageView.snp.right).offset(10)
+            make.centerY.equalTo(avatarImageView.snp.centerY)
+        }
+        
+        subTitleLable.snp.makeConstraints { (make) in
+            make.left.equalTo(titleLable.snp.right).offset(10)
+            make.centerY.equalTo(titleLable.snp.centerY)
+        }
+        
+        arrowImageView.snp.makeConstraints { (make) in
+            make.centerY.equalTo(avatarImageView.snp.centerY)
+            make.right.equalTo(self).offset(-15)
+            make.height.equalTo(10)
+            make.width.equalTo(7)
+        }
+    }
+    
+    fileprivate lazy var avatarImageView: UIImageView = {
+        let avatarImageView = UIImageView()
+        avatarImageView.backgroundColor = UIColor.red
+        return avatarImageView
+    }()
+    
+    fileprivate lazy var titleLable: UILabel = {
+        let titleLable = UILabel()
+        titleLable.font = UIFont.systemFont(ofSize: 16)
+        titleLable.text = "我的相册"
+        return titleLable
+    }()
+    
+    fileprivate lazy var subTitleLable: UILabel = {
+        let subTitleLable = UILabel()
+        subTitleLable.font = UIFont.systemFont(ofSize: 12)
+        subTitleLable.textColor = UIColor.lightGray
+        subTitleLable.text = "全民热舞抢红包"
+        return subTitleLable
+    }()
+    
+    fileprivate lazy var arrowImageView: UIImageView = {
+        let arrowImageView = UIImageView()
+        arrowImageView.image = UIImage(named: "SignIn_arrow")
+        return arrowImageView
+    }()
 }
