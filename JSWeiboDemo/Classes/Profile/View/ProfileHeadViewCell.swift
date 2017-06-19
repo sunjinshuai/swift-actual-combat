6be3e9dab1f5b06b34802f394f92e13689253cb4
孙金帅 <s_mike@163.com>
2017-06-19 19:13:06 +0800

    个人中心

---
+++ JSWeiboDemo/Classes/Profile/View/ProfileHeadViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileHeadViewCell.swift
+//  JSWeiboDemo
+//
+//  Created by michael on 2017/6/19.
+//  Copyright © 2017年 com.51fanxing. All rights reserved.
+//
+
+import UIKit
+
+class ProfileHeadViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ JSWeiboDemo/Classes/Profile/View/ProfileViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileViewCell.swift
+//  JSWeiboDemo
+//
+//  Created by michael on 2017/6/19.
+//  Copyright © 2017年 com.51fanxing. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
